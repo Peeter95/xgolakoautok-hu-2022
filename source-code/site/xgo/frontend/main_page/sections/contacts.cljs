0e87b7a919a923361ac5596a98841de43e176b22
Peeter95 <szan.peter95@gmail.com>
2023-01-09 22:31:57 +0100

    Contacts update

--- source-code/site/xgo/frontend/main_page/sections/contacts.cljs
+++ source-code/site/xgo/frontend/main_page/sections/contacts.cljs
@@ -1,14 +1,54 @@
 
-(ns site.xgo.frontend.main-page.sections.contacts)
+(ns site.xgo.frontend.main-page.sections.contacts
+  (:require [re-frame.api :as r]
+            [string.api                   :as string]
+            [uri.api                      :as uri]
+            [site.components.frontend.api :as components]))
 
 ;; -----------------------------------------------------------------------------
 ;; -----------------------------------------------------------------------------
+
+(defn social-media-link
+  [link icon-class label]
+  [:a.kc-social-media-link {:href (uri/valid-uri link) :target "_blank" :title label}
+                           [:i {:class icon-class}]
+                           [:span (-> link (string/not-starts-with! "https://")
+                                           (string/not-starts-with! "http://")
+                                           (string/not-starts-with! "www."))]])
+
+(defn facebook-links
+  []
+  (let [facebook-links @(r/subscribe [:x.db/get-item [:website-contacts :handler/transfered-content :facebook-links]])]
+       (letfn [(f [links link] (conj links [social-media-link link [:fab :fa-facebook] "Tovább a Facebook-ra"]))]
+              (reduce f [:<>] facebook-links))))
+
+(defn instagram-links
+  []
+  (let [instagram-links @(r/subscribe [:x.db/get-item [:website-contacts :handler/transfered-content :instagram-links]])]
+    (letfn [(f [links link] (conj links [social-media-link link [:fab :fa-instagram] "Tovább a Instagram-ra"]))]
+           (reduce f [:<>] instagram-links))))
+
+(defn youtube-links
+  []
+  (let [youtube-links @(r/subscribe [:x.db/get-item [:website-contacts :handler/transfered-content :youtube-links]])]
+    (letfn [(f [links link] (conj links [social-media-link link [:fab :fa-youtube] "Tovább a Youtube-ra"]))]
+           (reduce f [:<>] youtube-links))))
+
+(defn social-media-links
+  []
+  [:<> [facebook-links]
+       [instagram-links]
+       [youtube-links]])
 
 (defn- contacts
   []
-  [:div {:class "xgo-section--body"}])
+  [:div {:class "xgo-section--body"}
+        ;; [components/contacts ::contacts {}]
+        [:div#xgo-social-media-links [social-media-links]]])
+
 
 (defn view
   []
   [:section {:id "xgo-contacts"}
-            [contacts]])
+            [contacts]
+            [:div#xgo-contacts--background]])
